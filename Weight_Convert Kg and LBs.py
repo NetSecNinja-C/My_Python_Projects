86cdd0c3abec52ad089b945a7bdaaa72b144191b
NetSecNinja-C <134190663+NetSecNinja-C@users.noreply.github.com>
2023-05-30 23:59:47 +0530

    Update Weight_Convert Kg and LBs.py

--- Weight_Convert Kg and LBs.py
+++ Weight_Convert Kg and LBs.py
@@ -1,11 +1,11 @@
 Weight = float(input("Weight: "))
 K_L = input("Kg or Lbs : ")
 
-if K_L.upper == str("Kg"):
+if K_L == str("Kg"):
     X = Weight * float(2.20462)
     print("Weight in Lbs : " + str(X))
 
-elif K_L.upper == str("Lbs"):
+elif K_L == str("Lbs"):
     Y = Weight / float(2.20462)
     print("Weight in Kg : " + str(Y))
 
